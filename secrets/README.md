a4612ff67dd38467e3e14e22b3172422260641da
Kevin <kk@sudo-i.net>
2020-12-25 18:05:37 +0100

    adding stuff

---
+++ secrets/README.md
@@ -0,0 +1,3 @@
+# secrets
+
+Deploy the master key on the k8s cluster manuell via ```./deploy_key.sh```
---
+++ secrets/deploy_key.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# More infos https://github.com/bitnami-labs/sealed-secrets/blob/master/docs/bring-your-own-certificates.md
+
+export PRIVATEKEY="mytls.key"
+export PUBLICKEY="mytls.crt"
+export NAMESPACE="k8s-base-secrets"
+export SECRETNAME="mycustomkeys"
+
+[ ! -f ./${PRIVATEKEY}] && echo Private does not exists && exit1
+[ ! -f ./${PUBLICKEY}] && echo Public does not exists && exit1
+
+kubectl -n "$NAMESPACE" create secret tls "$SECRETNAME" --cert="$PUBLICKEY" --key="$PRIVATEKEY"
+kubectl -n "$NAMESPACE" label secret "$SECRETNAME" sealedsecrets.bitnami.com/sealed-secrets-key=active
+
+kubectl -n  "$NAMESPACE" delete pod -l app.kubernetes.io/name=sealed-secrets
